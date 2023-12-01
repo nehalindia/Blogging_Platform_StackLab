a83228184b571878a14e9a039cd6fc4c7c131df5
kazmy <nehaluddin.dpe@gmail.com>
2023-12-01 19:28:20 +0530

    controller

--- src/controller/blogcontroller.js
+++ src/controller/blogcontroller.js
@@ -1,1 +1,71 @@
 const userModel = require('../model/blogModel')
+const { isValid,isValidRequestBody } = require('../validation/validator');
+
+
+const createBlog = async function(req,res){
+    try{
+
+        
+    }catch(error){
+        res.status(500).send({
+            status : false, 
+            message : error.message
+        })
+    }
+}
+
+
+const getBlog = async function(req,res){
+    try{
+
+    }catch(error){
+        res.status(500).send({
+            status : false,
+            message : error.message
+        })
+    }
+}
+
+
+const getBlogById = async function(req,res){
+    try{
+
+    }catch(error){
+        res.status(500).send({
+            status : false,
+            message : error.message
+        })
+    }
+}
+
+
+const updateBlog = async function(req,res){
+    try{
+
+    }catch(error){
+        res.status(500).send({
+            status : false,
+            message : error.message
+        })
+    }
+}
+
+
+const deleteBlog = async function(req,res){
+    try{
+
+    }catch(error){
+        res.status(500).send({
+            status : false,
+            message : error.message
+        })
+    }
+} 
+
+module.exports = {
+    createBlog,
+    getBlog,
+    getBlogById,
+    updateBlog,
+    deleteBlog
+}
--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const { createBlog,getBlog,getBlogById,updateBlog,deleteBlog } = require('../controller/blogcontroller')
 
+router.post('/createBlog', createBlog)
+router.get('/getblog', getBlog)
+router.get('/getblogbyid', getBlogById)
+router.post('/update', updateBlog)
+router.post('/delete', deleteBlog)
 
 module.exports = router
