5a9e22f14422cf922434ee4cab3a0f2bff5116a2
kazmy <nehaluddin.dpe@gmail.com>
2023-12-02 02:29:35 +0530

    fix code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { requestlog } = require('../src/middelware/authentication')
 app.use(express.json())
 app.use(express.urlencoded( {extended : true} ))
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser : true })
+mongoose.connect(process.env.MONGO_URI)
 .then(
     console.log(" DataBase Connected")
 )
